570b955336a0a6505bf10e9a4b7ccd374af6c142
lcastillo <luiseduardo.castillo305@comunidadunir.net>
2024-02-19 21:43:43 -0500

    se cambia nombre de la plataforma

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Header = () => {
     return (
         <header className="header-footer">
-            <h1 className="header-footer-text">Plataforma de streaming online UNIR</h1>
+            <h1 className="header-footer-text">Plataforma de streaming online UNIR Laboratorio</h1>
         </header>
     )
 }
